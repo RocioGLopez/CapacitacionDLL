d30048cdb0ae86ec933e5b96cca1ecb48983f7d1
Rocio López <guisell.alvarado@gmail.com>
2025-09-05 19:18:29 -0600

    Creacion de dll, coboBox arrastrable

---
+++ EjecucionDll/EjecucionDll/Form1.Designer.cs
@@ -0,0 +1,70 @@
+﻿
+namespace EjecucionDll
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.combo1 = new CapaVista.Combo();
+            this.combo2 = new CapaVista.Combo();
+            this.SuspendLayout();
+            // 
+            // combo1
+            // 
+            this.combo1.Location = new System.Drawing.Point(27, 48);
+            this.combo1.Name = "combo1";
+            this.combo1.Size = new System.Drawing.Size(340, 33);
+            this.combo1.TabIndex = 0;
+            // 
+            // combo2
+            // 
+            this.combo2.Location = new System.Drawing.Point(397, 48);
+            this.combo2.Name = "combo2";
+            this.combo2.Size = new System.Drawing.Size(340, 33);
+            this.combo2.TabIndex = 1;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.LightBlue;
+            this.ClientSize = new System.Drawing.Size(800, 278);
+            this.Controls.Add(this.combo2);
+            this.Controls.Add(this.combo1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private CapaVista.Combo combo1;
+        private CapaVista.Combo combo2;
+    }
+}
+
